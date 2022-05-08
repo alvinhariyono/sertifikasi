afdbdcfadd4fa2fcd1161d6cbc67b1ad283e5db0
alvinhariyono <alvinhariyono1227@gmail.com>
2022-05-08 15:19:19 +0700

    new update code

--- view/delete.php
+++ view/delete.php
@@ -1,13 +1,12 @@
 <?php
 // include database connection file
-include_once("config.php");
- 
+include_once("../model/config.php");
+
 // Get id from URL to delete that user
-$id = $_GET['id'];
- 
+$stok_id = $_GET['stok_id'];
+
 // Delete user row from table based on given id
-$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
- 
+$result = mysqli_query($mysqli, "DELETE FROM stok WHERE stok_id=$stok_id");
+
 // After delete redirect to Home, so that latest user list will be displayed.
-header("Location:index.php");
-?>
+header("Location: mainpage.php");
--- view/edit.php
+++ view/edit.php
@@ -5,38 +5,43 @@ include_once("../model/config.php");
 
 // Check if form is submitted for user update, then redirect to homepage after update
 if (isset($_POST['update'])) {
-
-
 	$stok_id = $_POST['stok_id'];
 	$stok_nama = $_POST['stok_nama'];
-	$stok_kode = $_POST['stok_kode '];
+	$stok_kode = $_POST['stok_kode'];
 	$stok_status = $_POST['stok_status'];
-	$stok_peminjam = $_POST['stok_peminjam '];
+	$stok_peminjam = $_POST['stok_peminjam'];
 	$tanggal_peminjaman = $_POST['tanggal_peminjaman'];
 	$tanggal_pengembalian = $_POST['tanggal_pengembalian'];
 
-	// update user data
-	$result = mysqli_query($mysqli, "UPDATE users SET name='$name',email='$email',mobile='$mobile' WHERE id=$id");
+	// update book data
+	$result = mysqli_query(
+		$mysqli,
+		"UPDATE stok SET stok_id='$stok_id',stok_nama='$stok_nama',stok_kode='$stok_kode' ,
+	stok_status='$stok_status' ,stok_peminjam='$stok_peminjam',tanggal_peminjaman='$tanggal_peminjaman',
+	tanggal_pengembalian ='$tanggal_pengembalian ' WHERE stok_id=$stok_id"
+	);
 
 	// Redirect to homepage to display updated user in list
 	header("Location: mainpage.php");
 }
 ?>
 <?php
-// Display selected user data based on id
+// Display selected book data based on id
 // Getting id from url
-$id = $_GET['stok_id'];
+session_start();
+include_once("../model/config.php");
+$stok_id = $_GET['stok_id'];
 
 // Fetech user data based on id
-$result = mysqli_query($mysqli, "SELECT * FROM stok WHERE stok_id=$id");
+$result = mysqli_query($mysqli, "SELECT * FROM stok WHERE stok_id=$stok_id");
 
 
 while ($book_data = mysqli_fetch_array($result)) {
 	$stok_id = $book_data['stok_id'];
 	$stok_nama = $book_data['stok_nama'];
-	$stok_kode = $book_data['stok_kode '];
+	$stok_kode = $book_data['stok_kode'];
 	$stok_status = $book_data['stok_status'];
-	$stok_peminjam = $book_data['stok_peminjam '];
+	$stok_peminjam = $book_data['stok_peminjam'];
 	$tanggal_peminjaman = $book_data['tanggal_peminjaman'];
 	$tanggal_pengembalian = $book_data['tanggal_pengembalian'];
 }
@@ -58,49 +63,43 @@ while ($book_data = mysqli_fetch_array($result)) {
 </head>
 
 <body>
-	<a href="index.php">back to Home Page</a>
+	<a href="mainpage.php">back to Home Page</a>
 	<br /><br />
 
 	<form name="update_user" method="post" action="edit.php">
 		<table border="0">
 			<tr>
 				<td>stok_id</td>
-				<td><input type="text" name="name" value=<?php echo $stok_id; ?>></td>
+				<td><input type="text" size=100 name="stok_id" value="<?php echo $stok_id; ?>"></td>
 			</tr>
 			<tr>
 				<td>stok_nama</td>
-				<td><input type="text" name="email" value=<?php echo $stok_nama; ?>></td>
+				<td><input type="text" size=100 name="stok_nama" value="<?php echo $stok_nama; ?>"></td>
 			</tr>
 			<tr>
 				<td>stok_kode</td>
-				<td><input type="text" name="mobile" value=<?php echo $stok_kode; ?>></td>
+				<td><input type="text" size=100 name="stok_kode" value="<?php echo $stok_kode; ?>"></td>
 			</tr>
 			<tr>
 				<td>stok_status</td>
-				<td><input type="text" name="name" value=<?php echo $stok_status; ?>></td>
+				<td><input type="text" size=100 name="stok_status" value="<?php echo $stok_status; ?>"></td>
 			</tr>
 			<tr>
 				<td>stok_peminjam</td>
-				<td><input type="text" name="name" value=<?php echo $stok_peminjam; ?>></td>
+				<td><input type="text" size=100 name="stok_peminjam" value="<?php echo $stok_peminjam; ?>"></td>
 			</tr>
 			<tr>
 				<td>tanggal_peminjaman</td>
-				<td><input type="text" name="name" value=<?php echo $tanggal_peminjaman; ?>></td>
+				<td><input type="text" size=100 name="tanggal_peminjaman" value="<?php echo $tanggal_peminjaman; ?>"></td>
 			</tr>
 			<tr>
 				<td>tanggal_pengembalian</td>
-				<td><input type="text" name="name" value=<?php echo $tanggal_pengembalian; ?>></td>
+				<td><input type="text" size=100 name="tanggal_pengembalian" value="<?php echo $tanggal_pengembalian; ?>"></td>
 			</tr>
 
-
-
-
-
-
-
 			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id']; ?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
+				<!-- <td><input type="hidden" name="stok_id" value="<?php echo $_GET['stok_id']; ?>"></td> -->
+				<td><input type="submit" name="update" value="update"></td>
 			</tr>
 		</table>
 	</form>
